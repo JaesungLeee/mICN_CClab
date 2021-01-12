976c00105934e26a82c49139da9c82803a80e9f1
JaesungLeee <viki0114@naver.com>
2021-01-12 22:17:55 +0900

    [UPDATE] update emergency UI : Create all buttons

--- mICN/app/src/main/java/com/example/micnjs/UI/emergency.kt
+++ mICN/app/src/main/java/com/example/micnjs/UI/emergency.kt
@@ -3,10 +3,15 @@ package com.example.micnjs.UI
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.micnjs.R
+import kotlinx.android.synthetic.main.activity_emergency.*
 
 class emergency : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_emergency)
+
+        moveBack_btn.setOnClickListener {
+            finish()
+        }
     }
 }
