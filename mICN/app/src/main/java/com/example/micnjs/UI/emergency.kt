9b35e7cfa2c5b547be751d86f3aa8f01de378d5e
JaesungLeee <viki0114@naver.com>
2021-01-12 22:52:24 +0900

    [UPDATE] update emergency activity : emergency call not completed

--- mICN/app/src/main/java/com/example/micnjs/UI/emergency.kt
+++ mICN/app/src/main/java/com/example/micnjs/UI/emergency.kt
@@ -1,9 +1,17 @@
 package com.example.micnjs.UI
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.core.app.ActivityCompat
 import com.example.micnjs.R
 import kotlinx.android.synthetic.main.activity_emergency.*
+import java.lang.Exception
 
 class emergency : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,5 +21,27 @@ class emergency : AppCompatActivity() {
         moveBack_btn.setOnClickListener {
             finish()
         }
+
+        callEmergency_btn.setOnClickListener {
+            callEmergency()
+        }
+    }
+
+    private fun callEmergency() {
+        val intent = Intent(Intent.ACTION_CALL)
+        intent.setData(Uri.parse("tel:01028053454"))
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (ActivityCompat.checkSelfPermission(applicationContext, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                applicationContext.startActivity(intent)
+            }
+            else {
+                Log.e("CALLEMERGENCY", "No Permission")
+            }
+        }
+        else {
+            applicationContext.startActivity(intent)
+        }
+
     }
 }
